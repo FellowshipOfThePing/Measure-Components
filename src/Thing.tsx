0eaa86c9f08b59044fffd6c67cac5c41dde90b52
Jeremy <jeremy@neurophate.com>
2021-05-19 12:15:24 -0700

    End of dev.to tutorial

--- src/Thing.tsx
+++ src/Thing.tsx
@@ -14,7 +14,7 @@ export interface Props extends HTMLAttributes<HTMLDivElement> {
  */
 export const Thing: FC<Props> = ({ children }) => {
   return (
-    <div className="flex items-center justify-center w-5/6 m-auto text-2xl text-center text-pink-700 uppercase bg-blue-300 shadow-xl rounded-3xl">
+    <div className="flex items-center justify-center w-5/6 m-auto text-2xl text-center text-pink-700 uppercase bg-yellow-300 shadow-xl rounded-3xl">
       {children || `the snozzberries taste like snozzberries`}
     </div>
   );
